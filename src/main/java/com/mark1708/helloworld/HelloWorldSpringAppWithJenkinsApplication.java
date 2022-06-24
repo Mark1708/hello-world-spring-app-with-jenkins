867b044974e6155b69e9893d48f63926ab42e75d
Mark <m.gurianov@mail.ru>
2022-06-24 19:48:58 +0300

    modified:   pom.xml 	modified:   src/main/java/com/mark1708/helloworld/HelloWorldSpringAppWithJenkinsApplication.java 	modified:   src/test/java/com/mark1708/helloworld/HelloWorldSpringAppWithJenkinsApplicationTests.java

--- src/main/java/com/mark1708/helloworld/HelloWorldSpringAppWithJenkinsApplication.java
+++ src/main/java/com/mark1708/helloworld/HelloWorldSpringAppWithJenkinsApplication.java
@@ -3,10 +3,21 @@ package com.mark1708.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class HelloWorldSpringAppWithJenkinsApplication {
 
+    public static Logger logger = Logger.getLogger(HelloWorldSpringAppWithJenkinsApplication.class.getName());
+
+    @PostConstruct
+    public void init() {
+        logger.info("Application started...");
+    }
+
     public static void main(String[] args) {
+        logger.info("Application executed...");
         SpringApplication.run(HelloWorldSpringAppWithJenkinsApplication.class, args);
     }
 
--- src/test/java/com/mark1708/helloworld/HelloWorldSpringAppWithJenkinsApplicationTests.java
+++ src/test/java/com/mark1708/helloworld/HelloWorldSpringAppWithJenkinsApplicationTests.java
@@ -1,13 +1,20 @@
 package com.mark1708.helloworld;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.logging.Logger;
 
 @SpringBootTest
 class HelloWorldSpringAppWithJenkinsApplicationTests {
 
+    public static Logger logger = Logger.getLogger(HelloWorldSpringAppWithJenkinsApplicationTests.class.getName());
+
     @Test
-    void contextLoads() {
+   public void contextLoads() {
+        logger.info("Test case executing ...");
+        Assertions.assertEquals(true, true);
     }
 
 }
